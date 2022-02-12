9f4f8bf4ebcb81566c0d6eedee32655c74d53234
louisdumont <louisdumontbarbosa@protonmail.com>
2022-02-12 13:06:18 -0500

    added animation for project cards

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
 module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['cdn.tuk.dev','images.unsplash.com'],
   },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')]
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/aspect-ratio")],
+    mode: 'jit',
+    purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [require("@tailwindcss/aspect-ratio"),
+        require('tailwindcss'),
+        require('precss'),
+        require('autoprefixer')],
 };
